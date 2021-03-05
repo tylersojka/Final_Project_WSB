e349624afe9396ac89b270db118dbd5827c67b50
tsojka <sojkasojkasojka@gmail.com>
2021-03-04 18:57:48 -0600

    testing flask app build

--- Flask_app/app.py
+++ Flask_app/app.py
@@ -7,32 +7,21 @@ from Postgres.config import config
 from flask_sqlalchemy import SQLAlchemy
 
 
-
 # Obtain the configuration parameters
 params = config()
-
-
-
 
 #set up flask
 app = Flask(__name__)
 
-#use flask_sqlalchemy to set up postgres connection
-app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{params['user']}:{params['password']}@{params['host']}/{params['production_database']}"  #connect to postgres 
-pg = SQLAlchemy(app)
 
+#use flask_sqlalchemy to set up postgres connection
+# app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{params['user']}:{params['password']}@{params['host']}/{params['production_database']}"  #connect to postgres 
+# pg = SQLAlchemy(app)
 
 
 @app.route("/")
 def index():
    return render_template("index.html")
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
